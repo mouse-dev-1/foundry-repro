a16cc2475fdfa2d9d59474c0c9ae8101a87a294e
mousedev <anonymicenft@gmail.com>
2023-06-08 16:41:43 -0400

    Change

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
-import {Counter} from "../src/Counter.sol";
+contract Counter {
+    uint256 public number;
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
+
 
 contract CounterTest is Test {
     Counter public counter2;
--- test/Counter1.t.sol
+++ test/Counter1.t.sol
@@ -4,10 +4,21 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
-import {Counter} from "../src/Counter.sol";
+contract Counter {
+    uint256 public number;
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
+
 
 contract CounterTest is Test {
-    Counter public counter1;
+    Counter public counter2;
 
     function testCreationCode() public {
         console.logBytes32(keccak256(type(Counter).creationCode));
